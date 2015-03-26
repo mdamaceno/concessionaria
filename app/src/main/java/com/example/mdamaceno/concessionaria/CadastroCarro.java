979da5f72c4bed3f96950a73b12eb6ffd7910329
Marco Damaceno <maadamaceno@gmail.com>
2015-03-26 03:20:44 -0300

    Primeiro commit

---
+++ app/src/main/java/com/example/mdamaceno/concessionaria/CadastroCarro.java
@@ -0,0 +1,17 @@
+package com.example.mdamaceno.concessionaria;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+/**
+ * Created by mdamaceno on 25/03/15.
+ */
+public class CadastroCarro extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastro_carro);
+    }
+}
---
+++ app/src/main/java/com/example/mdamaceno/concessionaria/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.mdamaceno.concessionaria;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private EditText edtUsuario;
+    private EditText edtSenha;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void entrarOnClick(View view) {
+        edtUsuario = (EditText) findViewById(R.id.edtUsuario);
+        edtSenha = (EditText) findViewById(R.id.edtSenha);
+
+        String usuarioInformado = edtUsuario.getText().toString();
+        String senhaInformada = edtSenha.getText().toString();
+
+        if ("marco".equals(usuarioInformado) || "victor".equals(usuarioInformado) && "123".equals(senhaInformada)) {
+            startActivity(new Intent(this, CadastroCarro.class));
+        } else {
+            String mensagemErro = "Usuário ou senha inválidos!";
+            Toast toast = Toast.makeText(this, mensagemErro, Toast.LENGTH_LONG);
+            toast.show();
+        }
+    }
+}
